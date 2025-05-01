c70cc8e1064be84ef312470d222bd37b0cbbb38d
Mina Tharwat <minatharwat1923@gmail.com>
2025-05-02 01:54:50 +0300

    first commit

--- app.py
+++ app.py
@@ -1,28 +1,40 @@
 from flask import Flask, request, jsonify
-import numpy as np
 import tensorflow as tf
+import numpy as np
 from PIL import Image
 import io
 
 app = Flask(__name__)
+
+# Load the TFLite model
 interpreter = tf.lite.Interpreter(model_path="EfficientNetB3_skin_burn_model.tflite")
 interpreter.allocate_tensors()
 
+# Get input/output details
 input_details = interpreter.get_input_details()
 output_details = interpreter.get_output_details()
 
-@app.route("/predict", methods=["POST"])
+def preprocess_image(image_bytes):
+    image = Image.open(io.BytesIO(image_bytes)).convert("RGB")
+    image = image.resize((240, 240))  # Resize to model input size
+    img_array = np.array(image, dtype=np.float32) / 255.0
+    img_array = np.expand_dims(img_array, axis=0)
+    return img_array
+
+@app.route('/predict', methods=['POST'])
 def predict():
-    file = request.files['image']
-    img = Image.open(file.stream).resize((240, 240))  # Resize حسب الموديل
-    img = np.array(img).astype(np.float32) / 255.0
-    img = np.expand_dims(img, axis=0)
+    if 'image' not in request.files:
+        return jsonify({'error': 'No image uploaded'}), 400
+    image_file = request.files['image']
+    image_bytes = image_file.read()
+    input_data = preprocess_image(image_bytes)
 
-    interpreter.set_tensor(input_details[0]['index'], img)
+    interpreter.set_tensor(input_details[0]['index'], input_data)
     interpreter.invoke()
-    prediction = interpreter.get_tensor(output_details[0]['index'])
+    output_data = interpreter.get_tensor(output_details[0]['index'])
+    prediction = int(np.argmax(output_data))
 
-    return jsonify({"prediction": prediction.tolist()})
+    return jsonify({'prediction': prediction})
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    app.run(debug=True)
